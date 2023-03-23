8ae564ed869bb17b3c75b2484f7c6a08e4a7948b
Lewis Blake <lewisb@met.no>
2023-03-23 12:58:40 +0100

    remove settings.validators

--- tests/test_config.py
+++ tests/test_config.py
@@ -51,7 +51,6 @@ def test_settings(settings: Dynaconf):
 
 def test_settings_s3_bucket(secrets_s3_bucket: Path):
     settings = config(secrets=secrets_s3_bucket)
-    settings.validators.validate("s3_bucket")
     assert settings.s3_bucket.bucket_name == "s3_bucket_name"
     assert settings.s3_bucket.access_key_id == "access_key_id"
     assert settings.s3_bucket.secret_access_key == "secret_access_key"
