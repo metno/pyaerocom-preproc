8635b3824a0c8e83b8b23a173ab593bc126188c2
Lewis Blake <lewisb@met.no>
2023-03-23 10:19:25 +0100

    options.split()

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -112,5 +112,5 @@ def test_upload_obs():
 
 def test_bucket_ls():
     options = "bucket-ls"
-    result = runner.invoke(main, options)
+    result = runner.invoke(main, options.split())
     assert result.exit_code == 0
